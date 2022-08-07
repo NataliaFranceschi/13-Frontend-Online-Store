b8c2ce609f9b3b28ead11051937da848a47ed855
Arthur Barbosa <iarthu0@gmail.com>
2022-08-07 14:29:28 -0300

    Requisito 1.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { getCategories } from './services/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {/* asdadas */}
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        { getCategories() }
+        <p>asdasda</p>
+      </div>
+    );
+  }
 }
 
 export default App;
