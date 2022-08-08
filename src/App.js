e0a9de2eee53378e083344405b7eb68280cd0d82
Arthur Barbosa <iarthu0@gmail.com>
2022-08-08 19:34:55 -0300

    Requisito 8, não finalizado.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,69 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
-import Routes from './components/Routes';
-// import Home from './components/Home';
-// import { getCategories } from './services/api';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './Pages/Home';
+import Cart from './Pages/Cart';
+import ProductDetails from './Pages/ProductDetails';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      arrayCarrinho: [],
+    };
+  }
+
+  addCarrinho = (title, price, id) => {
+    const { arrayCarrinho } = this.state;
+    const produto = {
+      nome: title,
+      preço: price,
+      quantidade: 1,
+      id,
+    };
+    const newCarrinho = arrayCarrinho.map((product) => {
+      if (product.id === id) {
+        product.quantidade += 1;
+      }
+      return product;
+    });
+    if (!arrayCarrinho.some((item) => id === item.id)) {
+      this.setState((prevState) => ({
+        arrayCarrinho: [...prevState.arrayCarrinho, produto],
+      }));
+    } else { this.setState({ arrayCarrinho: newCarrinho }); }
+  }
+
   render() {
+    const { arrayCarrinho } = this.state;
     return (
       <BrowserRouter>
         <div className="App">
-          <Routes />
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={ () => (<Home
+                addCarrinho={ this.addCarrinho }
+              />) }
+            />
+            <Route
+              exact
+              path="/cart"
+              render={ () => (<Cart
+                arrayCarrinho={ arrayCarrinho }
+              />) }
+            />
+            <Route
+              exact
+              path="/productDetails/:id"
+              render={ (props) => (<ProductDetails
+                { ...props }
+                addCarrinho={ this.addCarrinho }
+              />
+              ) }
+            />
+          </Switch>
         </div>
       </BrowserRouter>
     );
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Cart extends React.Component {
   render() {
+    const { arrayCarrinho } = this.props;
+    const carrinhoVazio = (
+      <p data-testid="shopping-cart-empty-message">
+        Seu carrinho está vazio
+      </p>);
     return (
-      <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+      <div>
+        {
+          arrayCarrinho.length === 0
+            ? carrinhoVazio
+            : null
+        }
+      </div>
     );
   }
 }
+Cart.propTypes = {
+  arrayCarrinho: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+};
 
 export default Cart;
