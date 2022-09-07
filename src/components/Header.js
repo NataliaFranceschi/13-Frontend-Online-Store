38b8f9c07a19de46288d0c5eb8493b7f715dd18b
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-09-07 20:09:04 -0300

    CSS projeto

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,14 @@ import '../styles/header.scss';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { FaShoppingCart } from 'react-icons/fa';
+import OS from '../os.png';
 
 class Header extends Component {
   render() {
     const { quantidadeProdutos } = this.props;
     return (
       <div className="header">
-        <div>
-          <span className="online">O</span>
-          <span className="store">S</span>
-        </div>
-
+        <img src={ OS } alt="logo" />
         <Link
           data-testid="shopping-cart-button"
           to="/cart"
